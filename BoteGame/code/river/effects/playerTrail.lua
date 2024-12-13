98df0160893f53dcd7bda95d79e5ca2f55fe81dd
Quindocah <patrickhenschke07@hotmail.com>
2024-12-14 00:33:29 +1030

    Player particle trail

---
+++ BoteGame/code/river/effects/playerTrail.lua
@@ -0,0 +1,41 @@
+--windSpeed = 500
+trailAmount = 80
+
+function spawnTrail(dt)
+
+    trailTime = 1/trailAmount
+    if not trailCounter then trailCounter = 0 end
+
+    if trailCounter < trailTime then
+        trailCounter = trailCounter + dt
+    else
+        while trailCounter > trailTime do
+            createTrailParticle()
+            createTrailParticle() 
+           -- createTrailParticle((player.dir+math.pi)+2*(love.math.random(0,1)*2-1))
+            trailCounter = trailCounter - trailTime
+        end
+    end
+
+end
+
+function createTrailParticle(num)
+
+    local x = player.x + math.cos(player.dir)*-30
+    local y = player.y + math.sin(player.dir)*-30
+
+    if num then
+        x = player.x + math.cos(player.dir+math.sin(num)*0.2)*30
+        y = player.y + math.sin(player.dir+math.sin(num)*0.2)*30
+        angle = num + math.rad(love.math.random(-20,20))
+    else
+        angle = player.dir+math.pi + math.rad(love.math.random(-100,100))
+    end
+
+    print(player.dir)
+
+    particles.spawnParticle("trail",x,y,angle)
+    if love.math.random(1,5) == 1 then particles.spawnParticle("trail2",x,y,angle) end
+
+end
+
