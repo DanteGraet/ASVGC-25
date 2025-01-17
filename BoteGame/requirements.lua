24230278a889df233d7dbc5da83a19955d0e2b31
Danté <daniel.wataru.smith@gmail.com>
2025-01-17 23:30:55 +1030

    added audo thing

--- BoteGame/requirements.lua
+++ BoteGame/requirements.lua
@@ -5,8 +5,7 @@ tweens = require("templateLib/tweens")
 buttons = require("templateLib/graetUi")
 particles = require("templateLib/particle")
 --particles.loadParticleClasses = require("particleConf")
-
-require("templateLib/audioManager")
+audioPlayer = require("templateLib/audioManager")
 
 require("code/dynamicLoading")
 require("code/parallaxImage")
