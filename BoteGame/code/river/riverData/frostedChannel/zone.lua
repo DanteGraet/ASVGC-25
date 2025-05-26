af453d3f85591a72d5d68ee5980ec45dfde54dd6
Danté <daniel.wataru.smith@gmail.com>
2025-05-26 22:18:46 +0930

    Update zone.lua

--- BoteGame/code/river/riverData/frostedChannel/zone.lua
+++ BoteGame/code/river/riverData/frostedChannel/zone.lua
@@ -1,83 +1,3 @@
-local function iceplainsCurrent(percentage)
-    return 100*quindoc.clamp(percentage,0,1) + 100
-end
-
-local function boulderValleyDifficulty(percentage)
-    return 0.0015 + 0.0035*quindoc.clamp(percentage,0,1)
-end
-
-
-local function boulderValleyCurrent(percentage)
-    return 200*quindoc.clamp(percentage,0,1) + 100
-end
-
-local function boulderValleyChainLengthCoefficient(percentage)
-    return 1.35 - 0.2*quindoc.clamp(percentage,0,1)
-end
-
-
-
-
-local function boulderValleyDifficulty_STORM(percentage)
-    return 0.005
-end
-
-
-
-local function boulderValleyCurrent_STORM(percentage)
-
-    local idk = percentage
-
-    if percentage < 0.8 then
-
-        idk = percentage*1.25
-
-    else
-
-        idk = 1 - 2*((percentage - 0.8) * 5)
-
-    end
-
-    return 300 + 200*quindoc.clamp(idk,-0.5,1) 
-    
-end
-
-local function boulderValleyChainLengthCoefficient_STORM(percentage)
-    return 1.15 - 0.05*quindoc.clamp(percentage,0,1)
-end
-
-local function boulderValleyStormIntensity_STORM(percentage)
-
-    local idk = percentage
-
-    if percentage < 0.6 then
-
-        idk = percentage*1.66
-
-    elseif percentage < 0.8 then
-
-        idk = 1
-
-    else
-
-        idk = 1 - 2*((percentage - 0.8) * 5)
-
-    end
-
-    return 1000*quindoc.clamp(idk,0,1)
-end
-
-local function coniferousMountainsideDifficulty(percentage)
-
-    if percentage > 0.9 then
-        return 0
-    else
-        return 0.01
-    end
-
-end
-
-
 return {
     {
         zone = "icePlains",
@@ -85,9 +5,7 @@ return {
         distanceTitle = "Starting Point",
         subtitle = "River's Source",
         distance = 23000,
-        --difficultyFunction = 0.01,
         transition = 0,
-        --current = iceplainsCurrent,
         currentIcons = 2,
     },
     {
@@ -105,11 +23,8 @@ return {
         distanceTitle = "-- 1.5KM --",
         subtitle = "Rock-Chain Gully",
         distance = 15000,
-        --difficultyFunction = boulderValleyDifficulty,
         transition = 0,
-        --current = boulderValleyCurrent,
         currentIcons = 3,
-        --chainLengthCoefficient = boulderValleyChainLengthCoefficient,
     },
     {
         zone = "boulderValley", --yes i know there is a file for the storm version. not using it yet
@@ -117,12 +32,8 @@ return {
         subtitle = "Blizzard Approaching",
         distanceTitle = "-- 5KM --",
         distance = 49000,
-        --stormIntensity = boulderValleyStormIntensity_STORM,
-        --difficultyFunction = boulderValleyDifficulty_STORM,
         transition = 0,
-        --current = boulderValleyCurrent_STORM,
         currentIcons = 4,
-        --chainLengthCoefficient = boulderValleyChainLengthCoefficient_STORM,
     },
         {
         zone = "boulderValley-coniferousMountainside", --yes i know there is a file for the storm version. not using it yet
@@ -130,12 +41,8 @@ return {
         subtitle = "",
         distanceTitle = "",
         distance = 2000,
-        --stormIntensity = boulderValleyStormIntensity_STORM,
-        --difficultyFunction = boulderValleyDifficulty_STORM,
         transition = 0,
-        --current = boulderValleyCurrent_STORM,
         currentIcons = 4,
-        --chainLengthCoefficient = boulderValleyChainLengthCoefficient_STORM,
     },
     {
         zone = "coniferousMountainside",
@@ -143,15 +50,7 @@ return {
         displayName = "Coniferous Highlands",
         distanceTitle = "-- Almost There! --",
         distance = 9000,
-        --difficultyFunction = coniferousMountainsideDifficulty,
         transition = 0,
-        --current = 100,
         currentIcons = 2,
     },
-
-
 }
-
-
-
---riverGenerator:GetPercentageThrough(player.y)
