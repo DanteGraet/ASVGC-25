0d134a14c370f993a18a740602734fac7748d5b0
Danté <daniel.wataru.smith@gmail.com>
2025-07-09 14:20:36 +0930

    Update titleScreenButtons.lua

--- BoteGame/code/titleScreen/titleScreenButtons.lua
+++ BoteGame/code/titleScreen/titleScreenButtons.lua
@@ -17,32 +17,32 @@ function tsb.CreateButtons(UI)
     UI:GetButtons()["playButton"].functions.release =       {tsb.playButtonRelease}
 
     UI:GetButtons()["quitButton"]:AddText("Quit", "center", {"black", 100}, 12 - 870, 6, 1920, 1)
-    UI:GetButtons()["quitButton"]:AddImage(-100- 45/2, 45/2, love.graphics.newImage("image/titleScreen/titleIco3.png"), 1, 1)
-    UI:GetButtons()["quitButton"]:AddImage(-100- 45/2 + 6, 45/2+ 6, love.graphics.newImage("image/titleScreen/titleIco3.png"), 1, 1, 1)
+    UI:GetButtons()["quitButton"]:AddImage(-100- 45/2, 45/2 + 10, love.graphics.newImage("image/titleScreen/titleIco3.png"), 1, 1)
+    UI:GetButtons()["quitButton"]:AddImage(-100- 45/2 + 6, 45/2+ 6 + 10, love.graphics.newImage("image/titleScreen/titleIco3.png"), 1, 1, 1)
 
     UI:GetButtons()["quitButton"]:SetElementColour({0.0,0.0,0.0, 0.5}, nil, nil, 1)
     UI:GetButtons()["quitButton"]:SetElementColour({0.0,0.0,0.0, 0.5}, nil, nil, 2)
-    UI:GetButtons()["quitButton"]:SetElementColour({1,1,1}, {1, 0.6, 0.6}, {1, 0.4, 0.4})
+    --UI:GetButtons()["quitButton"]:SetElementColour({1,1,1}, {1, 0.6, 0.6}, {1, 0.4, 0.4})
 
 
     UI:GetButtons()["settingsButton"]:AddText("Settings", "center", {"black", 100}, 12 - 780, 6, 1920, 1)
-    UI:GetButtons()["settingsButton"]:AddImage(-100- 45/2, 45/2, love.graphics.newImage("image/titleScreen/titleIco2.png"), 1, 1)
-    UI:GetButtons()["settingsButton"]:AddImage(-100- 45/2 + 6, 45/2+ 6, love.graphics.newImage("image/titleScreen/titleIco2.png"), 1, 1, 1)
+    UI:GetButtons()["settingsButton"]:AddImage(-100- 45/2, 45/2 + 10, love.graphics.newImage("image/titleScreen/titleIco2.png"), 1, 1)
+    UI:GetButtons()["settingsButton"]:AddImage(-100- 45/2 + 6, 45/2+ 6+ 10, love.graphics.newImage("image/titleScreen/titleIco2.png"), 1, 1, 1)
 
     UI:GetButtons()["settingsButton"]:SetElementColour({0.0,0.0,0.0, 0.5}, nil, nil, 1)
     UI:GetButtons()["settingsButton"]:SetElementColour({0.0,0.0,0.0, 0.5}, nil, nil, 2)
-    UI:GetButtons()["settingsButton"]:SetElementColour({1,1,1}, {0.7, 0.7, 0.725}, {0.4, 0.4, 0.45})
+    --UI:GetButtons()["settingsButton"]:SetElementColour({1,1,1}, {0.7, 0.7, 0.725}, {0.4, 0.4, 0.45})
 
 
     UI:GetButtons()["playButton"]:AddText("Play", "center", {"black", 100}, 12 - 870, 6, 1920, 1)
-    UI:GetButtons()["playButton"]:AddImage(-100 - 45/2, 45/2, love.graphics.newImage("image/titleScreen/titleIco1.png"), 1, 1)
-    UI:GetButtons()["playButton"]:AddImage(-100- 45/2 + 6, 45/2+ 6, love.graphics.newImage("image/titleScreen/titleIco1.png"), 1, 1, 1)
+    UI:GetButtons()["playButton"]:AddImage(-100 - 45/2, 45/2 + 10, love.graphics.newImage("image/titleScreen/titleIco1.png"), 1, 1)
+    UI:GetButtons()["playButton"]:AddImage(-100- 45/2 + 6, 45/2+ 6 + 10, love.graphics.newImage("image/titleScreen/titleIco1.png"), 1, 1, 1)
 
     UI:GetButtons()["playButton"]:SetElementColour({0.0,0.0,0.0, 0.5}, nil, nil, 1)
     UI:GetButtons()["playButton"]:SetElementColour({0.0,0.0,0.0, 0.5}, nil, nil, 2)
-    UI:GetButtons()["playButton"]:SetElementColour({1,1,1}, {0.7, 0.7, 0.725}, {0.4, 0.4, 0.45})
+    --UI:GetButtons()["playButton"]:SetElementColour({1,1,1}, {0.7, 0.7, 0.725}, {0.4, 0.4, 0.45})
 
-    --boot leg fix
+    --bootleg fix
     local function fixButton(name)
         local b = UI:GetButtons()[name]
         local diff = 100 + 45/2
