20784d1d4ec7a63daba8c67c5cee2342dd86b25c
Danté <daniel.wataru.smith@gmail.com>
2025-07-16 13:16:58 +0930

    fix
    Error
    
    gameState/river.lua:156: attempt to index upvalue 'pauseMenu' (a nil value)
    
    Traceback
    
    [love "callbacks.lua"]:228: in function 'handler'
    gameState/river.lua:156: in function 'focus'
    main.lua:102: in function <main.lua:100>
    code/dynamicLoading.lua:147: in function 'Run'
    code/dynamicLoading.lua:36: in function 'New'
    gameState/river.lua:136: in function 'load'
    main.lua:198: in function 'updateGamestate'
    main.lua:206: in function 'update'
    [love "callbacks.lua"]:162: in function <[love "callbacks.lua"]:144>
    [C]: in function 'xpcall'

--- BoteGame/gameState/river.lua
+++ BoteGame/gameState/river.lua
@@ -151,7 +151,7 @@ function GetRiverScale()
 end
 
 local function focus(focus)
-    if player and not (player.health <= 0 or player.deathTime >= 1) then
+    if player and not (player.health <= 0 or player.deathTime >= 1) and pauseMenu then
 
         pauseMenu.isOpen = true
         pauseMenu.hasOpend = true
