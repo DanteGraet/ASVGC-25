fba015bc69e8c8adda155f2a1e4f49e602596575
Quindocah <61169005+Quindocah@users.noreply.github.com>
2025-07-19 12:49:44 +0930

    slight music changes

--- BoteGame/code/river/riverData/titleScreenRiver/music.lua
+++ BoteGame/code/river/riverData/titleScreenRiver/music.lua
@@ -2,7 +2,8 @@ return {
     data = {
         crossFadeSpeed = 0.3,
         tracks = {  -- Starting Values
-            [1] = {track = love.audio.newSource("music/mvpRiver/stormDrumsChordsV3.mp3","stream"),  volume = 0, targetVolume = 0},
+            [1] = {track = love.audio.newSource("music/autumnGrove/forestChords.mp3","stream"),  volume = 0.7, targetVolume = 0.7},
+            [2] = {track = love.audio.newSource("music/autumnGrove/forestStrings.mp3","stream"),  volume = 0.4, targetVolume = 0.4},
         },
     },
     zones = {
